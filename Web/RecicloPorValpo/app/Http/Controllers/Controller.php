5f18c5dbac5ae70272d6079a4dc704577f8b3d0a
Dionos <aldaner74@gmail.com>
2018-10-19 20:40:47 -0300

    0.03
    Cambio de name space

--- Web/RecicloPorValpo/app/Http/Controllers/Controller.php
+++ Web/RecicloPorValpo/app/Http/Controllers/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace RecicloPorValpo\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
