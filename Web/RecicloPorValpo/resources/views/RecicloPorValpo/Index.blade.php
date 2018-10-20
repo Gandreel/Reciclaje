27c0f52b5080d7ff1debd1fd8f9006f93d56073a
Dionos <aldaner74@gmail.com>
2018-10-19 22:14:52 -0300

    0.06
    Integracion de Boostrap

--- Web/RecicloPorValpo/resources/views/RecicloPorValpo/Index.blade.php
+++ Web/RecicloPorValpo/resources/views/RecicloPorValpo/Index.blade.php
@@ -1,3 +1,5 @@
+<!-- Integracion del Template -->
 @extends('RecicloPorValpo.layout.template')
+<!-- Lugar de posición del contenido -->
 @section('contenido')
 <h1>Hola Mundo</h1>
--- Web/RecicloPorValpo/resources/views/RecicloPorValpo/layout/template.blade.php
+++ Web/RecicloPorValpo/resources/views/RecicloPorValpo/layout/template.blade.php
@@ -2,8 +2,16 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>RecicloPorValpo</title>	
-	<body>	
+		<title>RecicloPorValpo</title>
+		
+		<!-- Integracion de Boostrap (CSS) -->
+		<link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" media="all"/>	
+	<body>
+		<!-- Lugar donde estara el contenido de la pagina -->
 		@yield('contenido')
+			
+		<!-- Integracion de Boostrap (js) -->
+		<script src="{{asset('js/bootstrap.min.js')}}"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	</body>
 </html>
